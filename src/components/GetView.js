6bf433423fc79d76033a5e4b405f3e9c6c6ac04e
Samuel Wright <wsright987@gmail.com>
2023-06-28 20:03:36 -0400

    Update GetView.js

--- src/components/GetView.js
+++ src/components/GetView.js
@@ -1,11 +1,11 @@
-import React, { Component, useLayoutEffect, useState, useEffect } from 'react';
+import React, { useLayoutEffect, useState, useEffect } from 'react';
 import Game from './Game';
 import GameManager from '../GameManager';
 
-function useWindowSize() {
+function useWindowSize () {
     const [size, setSize] = useState([0, 0]);
     useLayoutEffect(() => {
-      function updateSize() {
+      function updateSize () {
         setSize([window.innerWidth, window.innerHeight]);
       }
       window.addEventListener('resize', updateSize);
@@ -15,85 +15,7 @@ function useWindowSize() {
     return size;
   }
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
-}
-
-export function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return windowDimensions;
-}
-
-/*
-class GetView extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            
-        }
-        //this.viewRef = React.createRef()
-        this.getMediaQuery = this.getMediaQuery.bind(this)
-    }
-
-    getMediaQuery(){
-        return useMediaQuery({ query: `(max-width: 760px)` })
-    }
-
-    componentDidMount(){
-       console.log('window dimension', useWindowDimensions())
-    }
-
-    render() {
-        let viewStyle = {
-            height: 705,
-            //width: this.getMediaQuery ? '95vw': null,
-            margin: this.getMediaQuery ? 0 : 0,
-        }
-        let footerStyle = {
-            textAlign: 'center',
-            width: this.getMediaQuery ? '85vw' : 400,
-            marginTop: this.getMediaQuery ? 25 : 20,
-            margin: '5% auto',
-            color: '#C6BABD',
-        }
-
-        return (
-            <div className="view" 
-                style={viewStyle} 
-                ref={ref => !this.state.viewRef && this.setState({ viewRef: ref })}
-            >
-                <Game size={GameManager.size} viewRef={this.state.viewRef}/> 
-            
-                <div style={footerStyle}>
-                    <p style={{fontSize: '.75em'}}>                       
-                        <span>By Samuel Wright</span>
-                        &nbsp; &nbsp; &nbsp;
-                        <a href='https://github.com/Sleighs/Mint-2048' style={{color: '#C6BABD'}}>GitHub</a>
-                    </p>
-                </div>
-                
-            </div>
-                      
-        );
-    }   
-}
-*/
-
-const GetView = (props) => {
+const GetView = () => {
     const [windowWidth, windowHeight] = useWindowSize();
 
     useEffect(() => {
